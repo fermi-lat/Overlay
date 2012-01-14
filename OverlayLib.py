6908fe51d8c6d595d7aac1a784e7c763120d1e83
jrb <>
2012-01-14 01:38:13 +0000

    rationalize SCons dependencies

--- OverlayLib.py
+++ OverlayLib.py
@@ -1,20 +1,21 @@
-#$Header: $
+#$Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/Overlay/OverlayLib.py,v 1.1 2009/12/08 06:39:20 jrb Exp $
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library=['Overlay'])
-    env.Tool('TriggerLib')
-    env.Tool('EventLib')
-    env.Tool('LdfEventLib')
+
+    env.Tool('RootConvertLib')
+    env.Tool('rootUtilLib')
     env.Tool('OverlayEventLib')
-    env.Tool('RootIoLib')
     env.Tool('overlayRootDataLib')
-    env.Tool('TkrUtilLib')
-    env.Tool('AcdUtilLib')
-    env.Tool('GlastSvcLib')
     env.Tool('astroLib')
-    env.Tool('xmlBaseLib')
-    env.Tool('facilitiesLib')
+    ##env.Tool('configDataLib')   # doesn't help
+    env.Tool('xmlUtilLib')
+    env.Tool('AcdUtilLib')
+    env.Tool('CalUtilLib')
+    ###env.Tool('addLibrary', library = env['geant4Libs']) # neither does this
+
     env.Tool('addLibrary', library=env['gaudiLibs'])
+    #env.Tool('addLibrary', library=env['obfLibs'])
 
 def exists(env):
     return 1;
