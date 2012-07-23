57a4a7d9c610ab69892d7964d2d9ddb78f80956d
usher <>
2012-07-23 20:20:47 +0000

    working on resurrecting the test program, first round of updates to allow for testing on the linux side

--- src/test/OverlayTestAlg.cxx
+++ src/test/OverlayTestAlg.cxx
@@ -1,4 +1,4 @@
-// $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/Overlay/src/test/OverlayTestAlg.cxx,v 1.4.136.1 2010/09/20 16:15:42 heather Exp $
+// $Header: /nfs/slac/g/glast/ground/cvs/Overlay/src/test/OverlayTestAlg.cxx,v 1.5 2011/12/12 20:54:56 heather Exp $
 
 // Include files
 
@@ -8,6 +8,8 @@
 #include "GaudiKernel/GaudiException.h" 
 #include "GaudiKernel/SmartIF.h"
 #include "GaudiKernel/SmartDataPtr.h"
+#include "GaudiKernel/DataSvc.h"
+#include "GaudiKernel/GenericAddress.h"
 
 #include "Overlay/IOverlayDataSvc.h"
 
@@ -38,7 +40,12 @@ public:
     StatusCode finalize();
     
 private:
+    StatusCode setRootEvent();
+
     IOverlayDataSvc* m_overlayInputSvc;
+
+    /// Pointer to the overlay's data provider service
+    DataSvc*         m_dataSvc;
 };
 
 
@@ -80,6 +87,16 @@ StatusCode OverlayTestAlg::initialize()
         else                  log << MSG::INFO << "Input data service successfully retrieved" << endreq;
     }
 
+    IService* dataSvc = 0;
+    sc = service("OverlayDataSvc", dataSvc);
+    if (sc.isFailure() ) {
+        log << MSG::ERROR << "  can't get OverlayDataSvc " << endreq;
+        return sc;
+    }
+
+    // Caste back to the "correct" pointer
+    m_dataSvc = dynamic_cast<DataSvc*>(dataSvc);
+
     // Now look for the output data service
     if (service("OverlayOutputSvc", tmpService).isFailure())
     {
@@ -109,29 +126,39 @@ StatusCode OverlayTestAlg::execute()
     // For the test job it is necessary to set the time
     eventHeader->setTime(0);
 
+    // Since our overlay stuff is not stored in the /Event section of the TDS, we need 
+    // to explicitly "set the root" each event - or risk it not getting cleared. 
+    sc = setRootEvent();
+
+    if (sc.isFailure())
+    {
+        log << MSG::ERROR << "Clearing of the Overlay section of TDS failed" << endreq;
+        return sc;
+    }
+
     // Look up the EventOverlay object in the TDS
-    SmartDataPtr<Event::EventOverlay> event(eventSvc(), OverlayEventModel::Overlay::EventOverlay);
+    SmartDataPtr<Event::EventOverlay> event(m_dataSvc, m_dataSvc->rootName() + OverlayEventModel::Overlay::EventOverlay);
     if(!event)
     {
         log << MSG::INFO << "Could not find a EventOverlay object in the TDS" << endreq;
     }
 
     // First, recover CalOverlay objects, to see if we have anything to do
-    SmartDataPtr<Event::CalOverlayCol> calOverlayCol(eventSvc(), OverlayEventModel::Overlay::CalOverlayCol);
+    SmartDataPtr<Event::CalOverlayCol> calOverlayCol(m_dataSvc, m_dataSvc->rootName() + OverlayEventModel::Overlay::CalOverlayCol);
     if(!calOverlayCol)
     {
         log << MSG::INFO << "Could not find a CalOverlay object in the TDS" << endreq;
     }
 
     // Now recover any TkrOverlay objects, to see if we have anything to do
-    SmartDataPtr<Event::TkrOverlayCol> tkrOverlayCol(eventSvc(), OverlayEventModel::Overlay::TkrOverlayCol);
+    SmartDataPtr<Event::TkrOverlayCol> tkrOverlayCol(m_dataSvc, m_dataSvc->rootName() + OverlayEventModel::Overlay::TkrOverlayCol);
     if(!tkrOverlayCol)
     {
         log << MSG::INFO << "Could not find a CalOverlay object in the TDS" << endreq;
     }
 
     // Finally, recover any AcdOverlay objects, to see if we have anything to do
-    SmartDataPtr<Event::AcdOverlayCol> acdOverlayCol(eventSvc(), OverlayEventModel::Overlay::AcdOverlayCol);
+    SmartDataPtr<Event::AcdOverlayCol> acdOverlayCol(m_dataSvc, m_dataSvc->rootName() + OverlayEventModel::Overlay::AcdOverlayCol);
     if(!acdOverlayCol)
     {
         log << MSG::INFO << "Could not find a CalOverlay object in the TDS" << endreq;
@@ -147,6 +174,30 @@ StatusCode OverlayTestAlg::execute()
 StatusCode OverlayTestAlg::finalize() 
 {    
     return StatusCode::SUCCESS;
+}
+    
+StatusCode OverlayTestAlg::setRootEvent()
+{
+    StatusCode sc = StatusCode::SUCCESS;
+
+    // Set up the root event object
+    Event::EventOverlay overObj;
+
+    // Caste the data service into the input data service pointer
+    DataSvc* dataProviderSvc = dynamic_cast<DataSvc*>(m_dataSvc);
+
+    // Create a "simple" generic opaque address to go with this
+    IOpaqueAddress* refpAddress = new GenericAddress(EXCEL_StorageType,
+                                                     overObj.clID(),
+                                                     dataProviderSvc->rootName());
+
+    sc = dataProviderSvc->setRoot(dataProviderSvc->rootName(), refpAddress);
+
+    // This is the magic incantation to trigger the building of the directory tree...
+    SmartDataPtr<Event::EventOverlay> overHeader(dataProviderSvc, dataProviderSvc->rootName());
+    if (!overHeader) sc = StatusCode::FAILURE;
+
+    return sc;
 }
 
 
