5097f8e84f66bef758ed0e3621f8363fa6526584
jrb <>
2012-08-18 00:32:56 +0000

    support no-install headers on Windows

--- OverlayLib.py
+++ OverlayLib.py
@@ -1,7 +1,9 @@
-#$Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/Overlay/OverlayLib.py,v 1.1 2009/12/08 06:39:20 jrb Exp $
+#$Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/Overlay/OverlayLib.py,v 1.2 2012/01/14 01:38:13 jrb Exp $
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library=['Overlay'])
+        if env['PLATFORM']=='win32' and env.get('CONTAINERNAME','')=='GlastRelease':
+	    env.Tool('findPkgPath', package = 'Overlay') 
 
     env.Tool('RootConvertLib')
     env.Tool('rootUtilLib')
@@ -16,6 +18,11 @@ def generate(env, **kw):
 
     env.Tool('addLibrary', library=env['gaudiLibs'])
     #env.Tool('addLibrary', library=env['obfLibs'])
+    if env['PLATFORM']=='win32' and env.get('CONTAINERNAME','')=='GlastRelease':
+        env.Tool('findPkgPath', package = 'TkrUtil') 
+        env.Tool('findPkgPath', package = 'enums') 
+        env.Tool('findPkgPath', package = 'ntupleWriterSvc') 
+        env.Tool('findPkgPath', package = 'RootIo') 
 
 def exists(env):
     return 1;
